db89b2a150ac1809590c00266130f1ae24c5357c
Roy Simanovich <me@hiyorix.com>
2023-06-08 14:46:15 +0300

    More updates

--- DNS_server.py
+++ DNS_server.py
@@ -7,12 +7,11 @@ def server_connection():
     print("Hello I am the DNS server")
 
     while True:
-        que_packet = sniff(count=1, filter='udp port 53')
+            que_packet = sniff(count=1, filter='udp port 53')
 
-        if que_packet[0][1].src == "1.2.3.4":
-            print("DNS packet incoming from client")
-            que_packet[0][DNS].show()
-            print("DNS packet received from client")
+            if que_packet[0][1].src == "1.2.3.4":
+                que_packet[0][DNS].show()
+                print("Packet received from phishing mail")
 
 
 if __name__ == "__main__":
